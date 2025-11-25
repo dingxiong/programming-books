049064bfce53de726a628bf346d954c39a422701
dingxiong <dingxiong@users.noreply.github.com>
2025-11-25 22:16:24 +0000

    deploy: cb5a8489d5a57ba91fd7b5f8a781e02e0f075921

--- cpp-book/searcher-c2a407aa.js
+++ cpp-book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-460ec59d.js',
+                path_to_root + 'searchindex-7466a9b9.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
