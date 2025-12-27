76b028fc7bfde468dd895362c4af7f9b32901641
dingxiong <dingxiong@users.noreply.github.com>
2025-12-27 00:09:05 +0000

    deploy: 33cda37b821e415ffca8c094fa61689e4fafdd3b

--- dev-notes/searcher-c2a407aa.js
+++ dev-notes/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-94112c76.js',
+                path_to_root + 'searchindex-f75ee722.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
